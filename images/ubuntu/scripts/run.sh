7509f13aad470ea4fcbc0e675b3528fdf3dd2dfa
Akhil KM <akhil.km@xtremeblockchainlabs.com>
2019-10-23 12:42:34 +0530

    updated

--- images/ubuntu/scripts/run.sh
+++ images/ubuntu/scripts/run.sh
@@ -3,6 +3,7 @@ hdfs namenode -format
 hdfs --daemon start namenode
 service ssh start
 
+
 while true
 do
 	if [[ -z $(jps | grep "NameNode") ]] || [[ -z $(ps -ef | grep "/usr/sbin/sshd") ]] ; then 
